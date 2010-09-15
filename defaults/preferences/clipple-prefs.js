25462b5af6380542c768e65c4d0b6ac9fb58a5c2
mooz <stillpedant@gmail.com>
2010-09-16 01:18:00 +0900

    set default value of the preference

--- defaults/preferences/clipple-prefs.js
+++ defaults/preferences/clipple-prefs.js
@@ -1,5 +1,6 @@
 pref("extensions.clipple.limit_by_text_length", false);
 pref("extensions.clipple.text_length_max", 8190);
 pref("extensions.clipple.number_of_clipboards", 15);
+pref("extensions.clipple.use_paste_all", false);
 pref("extensions.clipple.ignore_password", true);
 pref("extensions.clipple.save_session", true);
